68d29c1a2df8d6f9ba69fa830b29adf1a703c504
José Padilla <jpadilla@webapplicate.com>
2017-04-19 00:20:36 -0400

    Fix CI

--- setup.py
+++ setup.py
@@ -8,22 +8,16 @@ from setuptools import setup
 
 install_requires = [
     'wagtail>=1.9,<2.0',
-    'jsonfield',
-    'filepreviews',
-    'django-model-utils'
+    'jsonfield>=2.0.1<3.0',
+    'filepreviews>=2.0.2,<3.0',
+    'django-model-utils>=3.0.0,<4.0'
 ]
 
 tests_require = [
-    'responses'
+    'responses>=0.5.1,<1.0',
+    'flake8>=3.3.0<4.0',
+    'isort>=4.2.5,<5.0'
 ]
-
-
-def read(*paths):
-    """
-    Build a file path from paths and return the contents.
-    """
-    with open(os.path.join(*paths), 'r') as f:
-        return f.read()
 
 
 def get_version(package):
@@ -56,7 +50,27 @@ setup(
     url='https://github.com/filepreviews/wagtail-filepreviews',
     packages=get_packages('wagtaildocs_previews'),
     license='MIT',
-    long_description=read('README.rst'),
+    long_description='See https://github.com/filepreviews/wagtail-filepreviews for details',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
     install_requires=install_requires,
-    tests_require=tests_require,
+    extra_requires={
+        'test': tests_require,
+    }
 )
